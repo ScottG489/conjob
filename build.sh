79c5e800ac943eaccc1c7aacb8e1aa14edafcb7b
Scott Giminiani <scottg489@gmail.com>
2019-10-27 21:30:17 -0700

    Use GitHub Actions and docker build server for build and deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-docker build -t foo .
-
-docker run foo echo hello
+curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'"}' 'https://diff-data.com/build?image=scottg489/docker-ci-prototype-build:latest'
