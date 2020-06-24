f59bc67470b807a15a0c1547fc24fa16dc62fa03
Scott G <scottg489@gmail.com>
2020-06-23 21:25:15 -0700

    Run build on alternate build server
    When the server builds and deploys itself it kills the existing server
    and starts up a new one. This means that the client waiting for a reply
    from the build server will have it's connection cut off. In order to
    prevent this from happening when building and deploying this project we
    will do it on an alternative server which is managed seprately.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' 'http://simple-ci.com/build?image=scottg489/docker-ci-prototype-build:latest'
+curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' 'http://ec2-34-211-180-243.us-west-2.compute.amazonaws.com/build?image=scottg489/docker-ci-prototype-build:latest'
