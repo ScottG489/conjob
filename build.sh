1b1ab37dbf54985d703015dba18a091b0a3bdd8e
Scott G <scottg489@gmail.com>
2020-06-27 20:03:36 -0700

    Split build script up onto multiple lines
    It's getting longer now so this will make it easier to read

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-curl -v -sS -w '%{http_code}' --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' 'http://ec2-34-211-180-243.us-west-2.compute.amazonaws.com/build?image=scottg489/docker-ci-prototype-build:latest' | tee /tmp/foo | sed '$d' && [ "$(tail -1 /tmp/foo)" -eq 200 ]
+curl -v -sS -w '%{http_code}' \
+  --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' \
+  'http://ec2-34-211-180-243.us-west-2.compute.amazonaws.com/build?image=scottg489/docker-ci-prototype-build:latest' \
+  | tee /tmp/foo \
+  | sed '$d' && \
+  [ "$(tail -1 /tmp/foo)" -eq 200 ]
