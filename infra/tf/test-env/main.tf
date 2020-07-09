6d7bf212b69736f4685dd5074bb88160d053ab1d
Scott G <scottg489@gmail.com>
2020-07-08 22:12:49 -0700

    Use a spot instance for the test environment
    - Use random values for resource identifiers. This prevents different
    runs of the build from conflicting with each other when running in
    parallel.
    - Refactor by pulling out values to variables

--- infra/tf/test-env/main.tf
+++ infra/tf/test-env/main.tf
@@ -2,19 +2,32 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "docker_ci_prototype_instance" {
+resource "aws_spot_instance_request" "spot_instance_request" {
   ami           = "ami-09dd2e08d601bff67"
-  instance_type = "t2.small"
-  vpc_security_group_ids = [aws_security_group.docker_ci_prototype_sg.id]
-  key_name = aws_key_pair.docker_ci_prototype_key.key_name
+  instance_type = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name = aws_key_pair.key_pair.key_name
+
+  spot_type = "one-time"
+  spot_price    = "0.03"
+  wait_for_fulfillment = true
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 8
+  }
 
   tags = {
-    Name = "docker-ci-prototype-test"
+    Name = "${var.spot_instance_tag_name}_${random_uuid.rand.result}"
+  }
+
+  provisioner "local-exec" {
+    command = "aws ec2 create-tags --resources ${aws_spot_instance_request.spot_instance_request.spot_instance_id} --tag --tags Key=Name,Value=${var.instance_tag_name}_${random_uuid.rand.result}"
   }
 }
 
-resource "aws_security_group" "docker_ci_prototype_sg" {
-  name = "docker-ci-prototype-sg-test"
+resource "aws_security_group" "sg" {
+  name = "${var.sg_name}_${random_uuid.rand.result}"
   ingress {
     from_port   = 0
     to_port     = 0
@@ -30,7 +43,9 @@ resource "aws_security_group" "docker_ci_prototype_sg" {
   }
 }
 
-resource "aws_key_pair" "docker_ci_prototype_key" {
-  key_name   = "docker-ci-prototype-key-test"
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.key_pair_name}_${random_uuid.rand.result}"
   public_key = var.public_key
 }
+
+resource "random_uuid" "rand" { }
--- infra/tf/test-env/variables.tf
+++ infra/tf/test-env/variables.tf
@@ -1,1 +1,5 @@
+variable "spot_instance_tag_name" {}
+variable "instance_tag_name" {}
+variable "sg_name" {}
+variable "key_pair_name" {}
 variable "public_key" {}
