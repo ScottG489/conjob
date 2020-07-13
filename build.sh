0357e515f3d2a5a0d2ac2586f222c00b29bd820f
Scott G <scottg489@gmail.com>
2020-07-12 18:06:57 -0700

    Update IP of bootstrap build server
    I think the spot instance died and another was restarted. Will have to
    look into this

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 curl -v -sS -w '%{http_code}' \
   --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' \
-  'http://ec2-54-245-40-45.us-west-2.compute.amazonaws.com/build?image=scottg489/docker-ci-prototype-build:latest' \
+  'http://18.237.98.51/build?image=scottg489/docker-ci-prototype-build:latest' \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
