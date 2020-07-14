e39096f29304cd462b96e3a8317accdf5be41e46
Scott G <scottg489@gmail.com>
2020-07-14 01:26:36 -0700

    Use new bootstrap build server url

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 curl -v -sS -w '%{http_code}' \
   --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' \
-  'http://18.237.98.51/build?image=scottg489/docker-ci-prototype-build:latest' \
+  'http://18.236.86.254/build?image=scottg489/docker-ci-prototype-build:latest' \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
