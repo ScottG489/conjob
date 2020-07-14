d3247781dbf452f989224b35a8bd702a5eb66c04
Scott G <scottg489@gmail.com>
2020-07-14 00:12:59 -0700

    Use ansible task to wait for service to become available
    The curl method was not working. This way is cleaner anyways.

--- infra/ansible/files/wait_for_server_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-sleep 30
-# Wait for service to come up before considering the playbook finished
-# IPv4 flag is required due to docker weirdness: https://github.com/appropriate/docker-curl/issues/5
-curl -sS --ipv4 --retry-connrefused --retry 5 localhost:80
