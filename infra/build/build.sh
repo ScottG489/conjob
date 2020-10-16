51c4707dea64e4734aaaa0f0592491917795a3a8
Scott Giminiani <scottg489@gmail.com>
2020-10-15 22:38:48 -0400

    Build using the dedicated url for the alt build server

--- infra/build/build.sh
+++ infra/build/build.sh
@@ -1,6 +1,6 @@
 curl -v -sS -w '%{http_code}' \
   --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "GIT_REPO_URL": "'"$GIT_REPO_URL"'", "RELATIVE_SUB_DIR": "'"$RELATIVE_SUB_DIR"'", "DOCKER_IMAGE_NAME": "'"$DOCKER_IMAGE_NAME"'"}' \
-  'http://18.236.86.254/build?image=scottg489/docker-build-push:latest' \
+  'http://alt.simple-ci.com/build?image=scottg489/docker-build-push:latest' \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
