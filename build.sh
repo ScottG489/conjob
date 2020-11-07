9f007f4d0abb283969c001af81f773bf8fe770db
Scott Giminiani <scottg489@gmail.com>
2020-11-07 14:53:06 -0500

    Refactor build scripts to be more readable

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
+#!/usr/bin/env bash
+
+readonly IMAGE_NAME='scottg489/docker-ci-prototype-build:latest'
+readonly ID_RSA=$1
+readonly DOCKER_CONFIG=$2
+readonly AWS_CREDENTIALS=$3
+readonly MAIN_KEY_PAIR=$4
+
+read -r -d '' JSON_BODY <<- EOM
+  {
+  "ID_RSA": "$ID_RSA",
+  "DOCKER_CONFIG": "$DOCKER_CONFIG",
+  "AWS_CREDENTIALS": "$AWS_CREDENTIALS",
+  "MAIN_KEY_PAIR": "$MAIN_KEY_PAIR"
+  }
+EOM
+
 curl -v -sS -w '%{http_code}' \
-  --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'"}' \
-  'http://alt.simple-ci.com/build?image=scottg489/docker-ci-prototype-build:latest' \
+  --data-binary "$JSON_BODY" \
+  "http://alt.simple-ci.com/build?image=$IMAGE_NAME" \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
--- infra/build/build.sh
+++ infra/build/build.sh
@@ -1,6 +1,22 @@
+#!/usr/bin/env bash
+
+readonly IMAGE_NAME='scottg489/docker-build-push:latest'
+readonly ID_RSA=$1
+readonly DOCKER_CONFIG=$2
+
+read -r -d '' JSON_BODY <<- EOM
+  {
+  "ID_RSA": "$ID_RSA",
+  "DOCKER_CONFIG": "$DOCKER_CONFIG",
+  "GIT_REPO_URL": "$GIT_REPO_URL",
+  "RELATIVE_SUB_DIR": "$RELATIVE_SUB_DIR",
+  "DOCKER_IMAGE_NAME": "$DOCKER_IMAGE_NAME"
+  }
+EOM
+
 curl -v -sS -w '%{http_code}' \
-  --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "GIT_REPO_URL": "'"$GIT_REPO_URL"'", "RELATIVE_SUB_DIR": "'"$RELATIVE_SUB_DIR"'", "DOCKER_IMAGE_NAME": "'"$DOCKER_IMAGE_NAME"'"}' \
-  'http://alt.simple-ci.com/build?image=scottg489/docker-build-push:latest' \
+  --data-binary "$JSON_BODY" \
+  "http://alt.simple-ci.com/build?image=$IMAGE_NAME" \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
