3c9be087603a9b82009c527a8681c63017ec3b6d
Scott Giminiani <scottg489@gmail.com>
2020-11-09 01:23:18 -0500

    Remove acceptance test dependency on project
    The tests were converting responses using models from the project. This
    was convenient but we don't want any coupling between the tests and the
    project. Otherwise changes in the project could directly affect results
    in the tests rather than affecting them indirectly when the test runs.

--- src/test/acceptance/java/dci/resource/BuildTest.java
+++ src/test/acceptance/java/dci/resource/BuildTest.java
@@ -1,20 +1,15 @@
 package dci.resource;
 
-import dci.api.JobResponse;
-import dci.api.JobResultResponse;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
 
 import static dci.util.RestAssuredUtil.configTest;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 public class BuildTest {
     @Before
@@ -26,33 +21,36 @@ public class BuildTest {
     public void getPlainTextResponse() {
         String expectContains = "Hello from Docker!";
 
-        ExtractableResponse<Response> response = RestAssured
-                .get("/build?image=library/hello-world:latest")
-                .then()
-                    .extract();
-
-        assertThat(response.contentType(), is(MediaType.TEXT_PLAIN));
-        assertThat(response.body().asString(), containsString(expectContains));
+        given()
+            .get("/build?image=library/hello-world:latest")
+        .then()
+            .contentType(MediaType.TEXT_PLAIN)
+            .body(containsString(expectContains));
     }
 
     @Test
     public void getJsonResponse() {
         String expectOutputContains = "Hello from Docker!";
 
-        ExtractableResponse<Response> response = RestAssured
-                .given()
-                    .accept(ContentType.JSON)
-                .get("/build?image=library/hello-world:latest")
-                .then()
-                    .extract();
+        given()
+            .accept(ContentType.JSON)
+            .get("/build?image=library/hello-world:latest")
+        .then()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body("jobRun.output", containsString(expectOutputContains))
+            .body("jobRun.exitCode", is(0))
+            .body("result", is("FINISHED"));
+    }
 
-        assertThat(response.contentType(), is(MediaType.APPLICATION_JSON));
-
-        JobResponse jobResponse = response.as(JobResponse.class);
-
-        assertThat(jobResponse.getJobRun().getOutput(), containsString(expectOutputContains));
-        assertThat(jobResponse.getJobRun().getExitCode(), is(0L));
-
-        assertThat(jobResponse.getResult(), is(JobResultResponse.FINISHED));
+    @Test
+    public void getJsonResponsea() {
+        given()
+            .accept(ContentType.JSON)
+            .get("/build?image=local/does_not_exist:latest")
+       .then()
+           .contentType(MediaType.APPLICATION_JSON)
+           .body("jobRun.output", containsString(""))
+           .body("jobRun.exitCode", is(-1))
+           .body("result", is("NOT_FOUND"));
     }
 }
