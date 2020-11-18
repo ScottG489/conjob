97c05caf0a7d0771dad4fad422f8a4dff0f1d65a
Scott Giminiani <scottg489@gmail.com>
2020-11-18 16:07:44 -0500

    Require tag to be specified
    It seems that if a tag isn't specified then it tries to pull down every
    single tag. This is definitely not desireable.
    
    This is a temporary fix until we make the change to default to "latest"
    tag if it isn't specified by the user.
    
    Perhaps this validation logic can still be reused lower down in the code
    just to make sure this doesn't happen before running the image.

--- src/main/java/conjob/resource/JobResource.java
+++ src/main/java/conjob/resource/JobResource.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.security.PermitAll;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,6 +15,9 @@ import javax.ws.rs.core.Response;
 @PermitAll
 @Slf4j
 public class JobResource {
+    // Taken from here: https://stackoverflow.com/a/39672069/14146969   Second to last char (?) removed to require
+    //   user to specify tag. See here for why: https://github.com/ScottG489/conjob/issues/18
+    private static final String DOCKER_IMAGE_NAME_FORMAT = "^(?:(?=[^:\\/]{1,253})(?!-)[a-zA-Z0-9-]{1,63}(?<!-)(?:\\.(?!-)[a-zA-Z0-9-]{1,63}(?<!-))*(?::[0-9]{1,5})?/)?((?![._-])(?:[a-z0-9._-]*)(?<![._-])(?:/(?![._-])[a-z0-9._-]*(?<![._-]))*)(?::(?![.-])[a-zA-Z0-9_.-]{1,128})$";
     private final JobService jobService;
 
     public JobResource(JobService jobService) {
@@ -22,32 +26,38 @@ public class JobResource {
 
     @POST
     @Produces({MediaType.WILDCARD, MediaType.TEXT_PLAIN})
-    public Response handlePost(@NotEmpty @QueryParam("image") String imageName, String input,
-                               @QueryParam("pull") @DefaultValue("always") String pullStrategy)
+    public Response handlePost(
+            @NotEmpty @Pattern(regexp = DOCKER_IMAGE_NAME_FORMAT) @QueryParam("image") String imageName,
+            String input,
+            @QueryParam("pull") @DefaultValue("always") String pullStrategy)
             throws DockerException, InterruptedException {
         return createResponse(imageName, input, pullStrategy);
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response handleJsonPost(@NotEmpty @QueryParam("image") String imageName, String input,
-                                   @QueryParam("pull") @DefaultValue("always") String pullStrategy)
+    public Response handleJsonPost(
+            @NotEmpty @Pattern(regexp = DOCKER_IMAGE_NAME_FORMAT) @QueryParam("image") String imageName,
+            String input,
+            @QueryParam("pull") @DefaultValue("always") String pullStrategy)
             throws DockerException, InterruptedException {
         return createJsonResponse(imageName, input, pullStrategy);
     }
 
     @GET
     @Produces({MediaType.WILDCARD, MediaType.TEXT_PLAIN})
-    public Response handleGet(@NotEmpty @QueryParam("image") String imageName,
-                              @QueryParam("pull") @DefaultValue("always") String pullStrategy)
+    public Response handleGet(
+            @NotEmpty @Pattern(regexp = DOCKER_IMAGE_NAME_FORMAT) @QueryParam("image") String imageName,
+            @QueryParam("pull") @DefaultValue("always") String pullStrategy)
             throws DockerException, InterruptedException {
         return createResponse(imageName, null, pullStrategy);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response handleJsonGet(@NotEmpty @QueryParam("image") String imageName,
-                                  @QueryParam("pull") @DefaultValue("always") String pullStrategy)
+    public Response handleJsonGet(
+            @NotEmpty @Pattern(regexp = DOCKER_IMAGE_NAME_FORMAT) @QueryParam("image") String imageName,
+            @QueryParam("pull") @DefaultValue("always") String pullStrategy)
             throws DockerException, InterruptedException {
         return createJsonResponse(imageName, null, pullStrategy);
     }
