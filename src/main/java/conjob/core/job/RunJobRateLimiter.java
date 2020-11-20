9bb13774d78e5e62c3c84237044f1f541d5e7cf9
Scott Giminiani <scottg489@gmail.com>
2020-11-20 02:17:19 -0500

    Use limiter config values directly from the config
    This is a precursor to the upcoming runtime configuration feature

--- src/main/java/conjob/core/job/RunJobRateLimiter.java
+++ src/main/java/conjob/core/job/RunJobRateLimiter.java
@@ -6,21 +6,19 @@ import conjob.config.JobConfig;
 import java.util.concurrent.TimeUnit;
 
 public class RunJobRateLimiter {
-    private final long MAX_RUN_JOB_REQUESTS_IN_WINDOW;
-    private final long MAX_RUNNING_JOBS_COUNT;
+    private final JobConfig.LimitConfig limitConfig;
 
     private final SlidingTimeWindowReservoir requestTimeWindow =
             new SlidingTimeWindowReservoir(1, TimeUnit.SECONDS);
     private int runningJobsCount = 0;
 
     public RunJobRateLimiter(JobConfig.LimitConfig limitConfig) {
-        MAX_RUN_JOB_REQUESTS_IN_WINDOW = limitConfig.getMaxGlobalRequestsPerSecond();
-        MAX_RUNNING_JOBS_COUNT = limitConfig.getMaxConcurrentRuns();
+        this.limitConfig = limitConfig;
     }
 
     public synchronized boolean isAtLimit() {
-        if (runningJobsCount >= MAX_RUNNING_JOBS_COUNT
-                || requestTimeWindow.getSnapshot().getValues().length >= MAX_RUN_JOB_REQUESTS_IN_WINDOW) {
+        if (runningJobsCount >= limitConfig.getMaxConcurrentRuns()
+                || requestTimeWindow.getSnapshot().getValues().length >= limitConfig.getMaxGlobalRequestsPerSecond()) {
             return true;
         }
 
