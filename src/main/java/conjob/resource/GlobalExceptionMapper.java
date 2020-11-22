7ff12fec72262eb8817584671bd05a25f6bdc63d
Scott Giminiani <scottg489@gmail.com>
2020-11-22 12:53:27 -0500

    Log all stacktraces in global exception handler

--- src/main/java/conjob/resource/GlobalExceptionMapper.java
+++ src/main/java/conjob/resource/GlobalExceptionMapper.java
@@ -1,14 +1,18 @@
 package conjob.resource;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.core.Response;
 
 // This exception handler catches all exceptions thrown from our resources. This should only be a fallback and
 //   a best effort should be made to be aware of any exceptions that could be thrown and handle them explicitly.
 //   Any time this code is run it should be considered a bug and an explicit error handler should be written as a
 //   fix.
+@Slf4j
 public class GlobalExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception> {
     @Override
     public Response toResponse(Exception exception) {
+        exception.printStackTrace();
         return Response.serverError().build();
     }
 }
