d56917cd7ae18e2a5c6f1d88039e3d8f1bfc0b6d
Scott Giminiani <scottg489@gmail.com>
2020-12-19 01:03:26 -0500

    Implement method for creating a job config

---
+++ src/main/java/conjob/service/JobRunConfigCreator.java
@@ -0,0 +1,14 @@
+package conjob.service;
+
+public class JobRunConfigCreator {
+    public JobRunConfig getContainerConfig(String imageName, String input, String secretId) {
+        JobRunConfig jobRunConfig;
+        if (input != null && !input.isEmpty()) {
+            jobRunConfig = new JobRunConfig(imageName, input, secretId);
+        } else {
+            jobRunConfig = new JobRunConfig(imageName, null, secretId);
+        }
+
+        return jobRunConfig;
+    }
+}
