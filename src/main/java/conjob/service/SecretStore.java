4bd35c6dbfa6b6cf1982b7ebaad2acce8c882e3c
Scott Giminiani <scottg489@gmail.com>
2020-12-19 00:57:42 -0500

    Implement method for finding optional secret for job
    Not all jobs have a secret. This finds a secret if it exists.

---
+++ src/main/java/conjob/service/SecretStore.java
@@ -0,0 +1,23 @@
+package conjob.service;
+
+import com.spotify.docker.client.exceptions.DockerException;
+
+import java.util.Optional;
+
+public class SecretStore {
+    private final DockerAdapter dockerAdapter;
+
+    public SecretStore(DockerAdapter dockerAdapter) {
+        this.dockerAdapter = dockerAdapter;
+    }
+
+    public Optional<String> findSecret(String secretName) throws SecretStoreException {
+        try {
+            return dockerAdapter.listAllVolumeNames().stream()
+                    .filter(volName -> volName.equals(secretName))
+                    .findFirst();
+        } catch (DockerException | InterruptedException e) {
+            throw new SecretStoreException("Problem finding secret");
+        }
+    }
+}
---
+++ src/main/java/conjob/service/SecretStoreException.java
@@ -0,0 +1,7 @@
+package conjob.service;
+
+public class SecretStoreException extends Exception {
+    public SecretStoreException(String message) {
+        super(message);
+    }
+}
