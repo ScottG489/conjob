4ee999f846973bb3594e0a8749df1c094da1221c
Scott Giminiani <scottg489@gmail.com>
2021-03-07 13:09:27 -0800

    Upgrade from junit4 to junit5 for unit tests
    I belive junit4 may still be used for acceptance tests. Those will be
    updated separately if necessary. For now we'll still leave in the junit4
    dependency.

--- src/test/unit/java/conjob/ConJobApplicationTest.java
+++ src/test/unit/java/conjob/ConJobApplicationTest.java
@@ -1,12 +1,14 @@
 package conjob;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
 
 public class ConJobApplicationTest {
     @Test
+    @DisplayName("Application should be named ConJob")
     public void getName() {
         ConJobApplication app = new ConJobApplication();
         assertThat(app.getName(), is("ConJob"));
--- src/test/unit/java/conjob/healthcheck/VersionCheckTest.java
+++ src/test/unit/java/conjob/healthcheck/VersionCheckTest.java
@@ -1,13 +1,16 @@
 package conjob.healthcheck;
 
 import com.codahale.metrics.health.HealthCheck;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 public class VersionCheckTest {
-
-    @org.junit.Test
+    @Test
+    // TODO: This test isn't actually doing what is described here
+    @DisplayName("Version health check should return value specified in version.properties")
     public void check() throws Exception {
         VersionCheck versionCheck = new VersionCheck();
         HealthCheck.Result checkResult = versionCheck.check();
