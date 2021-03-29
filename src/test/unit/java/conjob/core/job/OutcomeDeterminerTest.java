543a9d2949d835c0bea49aab625355d67fe37779
Scott Giminiani <scottg489@gmail.com>
2021-03-28 17:15:05 -0700

    Change outcome determiner test to be property based

--- src/test/unit/java/conjob/core/job/OutcomeDeterminerTest.java
+++ src/test/unit/java/conjob/core/job/OutcomeDeterminerTest.java
@@ -4,12 +4,11 @@ import conjob.core.job.exception.UnknownOutcomeStatusCodeException;
 import conjob.core.job.model.JobRunConclusion;
 import conjob.core.job.model.JobRunOutcome;
 import net.jqwik.api.ForAll;
+import net.jqwik.api.Label;
 import net.jqwik.api.Property;
 import net.jqwik.api.constraints.LongRange;
 import net.jqwik.api.constraints.Positive;
 import net.jqwik.api.lifecycle.BeforeTry;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -18,26 +17,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class OutcomeDeterminerTest {
     private OutcomeDeterminer outcomeDeterminer;
 
-    @BeforeEach
     @BeforeTry
     void setUp() {
         outcomeDeterminer = new OutcomeDeterminer();
     }
 
-    @Test
-    void determineOutcomeSuccess() {
+    @Property
+    @Label("Given an exit status code of 0, " +
+            "when determining the conclusion, " +
+            "then the conclusion should be that it was successful.")
+    void determineOutcomeSuccess(@ForAll String output) {
         Long exitStatusCode = 0L;
-        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, "");
+        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, output);
 
         JobRunConclusion jobRunConclusion = outcomeDeterminer.determineOutcome(jobRunOutcome);
 
         assertThat(jobRunConclusion, is(JobRunConclusion.SUCCESS));
     }
 
-    @Test
-    void determineOutcomeTimedOut() {
+    @Property
+    @Label("Given an exit status code of -1, " +
+            "when determining the conclusion, " +
+            "then the conclusion should be that it timed out.")
+    void determineOutcomeTimedOut(@ForAll String output) {
         Long exitStatusCode = -1L;
-        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, "");
+        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, output);
 
         JobRunConclusion jobRunConclusion = outcomeDeterminer.determineOutcome(jobRunOutcome);
 
@@ -45,21 +49,28 @@ class OutcomeDeterminerTest {
     }
 
     @Property
-    boolean determineOutcomeFailure(@ForAll @Positive long exitStatusCode) {
-        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, "");
+    @Label("Given any positive exit status code, " +
+            "when determining the conclusion, " +
+            "then the conclusion should be that it failed.")
+    void determineOutcomeFailure(
+            @ForAll @Positive long exitStatusCode,
+            @ForAll String output) {
+        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, output);
 
         JobRunConclusion jobRunConclusion = outcomeDeterminer.determineOutcome(jobRunOutcome);
 
         assertThat(jobRunConclusion, is(JobRunConclusion.FAILURE));
-        return true;
     }
 
     @Property
-    boolean determineOutcomeUnknownOutcomeStatusCodeException(
-            @ForAll @LongRange(min = Integer.MIN_VALUE, max = -2) long exitStatusCode) {
-        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, "");
+    @Label("Given any exit status code less than -1, " +
+            "when determining the conclusion, " +
+            "then the conclusion should be that it is an illegal status code.")
+    void determineOutcomeUnknownOutcomeStatusCodeException(
+            @ForAll @LongRange(min = Integer.MIN_VALUE, max = -2) long exitStatusCode,
+            @ForAll String output) {
+        JobRunOutcome jobRunOutcome = new JobRunOutcome(exitStatusCode, output);
 
         assertThrows(UnknownOutcomeStatusCodeException.class, () -> outcomeDeterminer.determineOutcome(jobRunOutcome));
-        return true;
     }
 }
