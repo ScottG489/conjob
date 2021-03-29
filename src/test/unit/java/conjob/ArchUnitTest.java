7a7f73162539fad93e648c197697a6b4a299ba5d
Scott Giminiani <scottg489@gmail.com>
2021-03-28 18:19:19 -0700

    Simplify the architecture unit tests

--- src/test/unit/java/conjob/ArchUnitTest.java
+++ src/test/unit/java/conjob/ArchUnitTest.java
@@ -6,6 +6,9 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.tngtech.archunit.base.DescribedPredicate.doNot;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAnyPackage;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packages = "conjob")
@@ -13,19 +16,16 @@ public class ArchUnitTest {
     @Test
     @DisplayName("Given a class in the core library, " +
             "when it depends on another class, " +
-            "then the dependency should be in the core library.")
+            "then the dependency should be in the core library, " +
+            "or the dependency should be an external dependency.")
     public void coreLibraryShouldOnlyDependOnItself() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("conjob..");
 
         classes()
                 .that().resideInAPackage("conjob.core..")
-                .should().onlyDependOnClassesThat().resideInAnyPackage(
-                "conjob.core..",
-                "com..",
-                "org..",
-                "net..",
-                "java..",
-                "lombok..")
+                .should().onlyDependOnClassesThat(
+                resideInAPackage("conjob.core..")
+                        .or(doNot(resideInAPackage("conjob.."))))
                 .because("lower level libraries shouldn't have dependencies higher level ones.")
                 .check(importedClasses);
     }
@@ -34,21 +34,16 @@ public class ArchUnitTest {
     @DisplayName("Given a class in the service library, " +
             "when it depends on another class, " +
             "then the dependency should be in the same library " +
-            "or the dependency should only be in lower level libraries.")
+            "or the dependency should only be in lower level libraries, " +
+            "or the dependency should be an external dependency.")
     public void serviceLibraryShouldOnlyDependOnItselfAndCore() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("conjob..");
 
         classes()
                 .that().resideInAPackage("conjob.service..")
-                .should().onlyDependOnClassesThat().resideInAnyPackage(
-                "conjob.service..",
-                "conjob.core..",
-                "conjob.config..",
-                "com..",
-                "org..",
-                "net..",
-                "java..",
-                "lombok..")
+                .should().onlyDependOnClassesThat(
+                resideInAnyPackage("conjob.service..", "conjob.core..", "conjob.config..")
+                        .or(doNot(resideInAPackage("conjob.."))))
                 .because("lower level libraries shouldn't have dependencies are higher level ones.")
                 .check(importedClasses);
     }
