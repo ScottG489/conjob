28d3395db04ff58d7c6fccba78cfcecad91d975d
Scott Giminiani <scottg489@gmail.com>
2021-04-11 20:55:22 -0700

    Do test cleanup even if there's a failure
    The clean up at the end of the file was only happening if the tests
    finished successfully and we want the cleanup to happen regardless.
    
    - Also fix permissions on hidden files in the root directory

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
+trap cleanup EXIT
+cleanup() {
+  # The local fs is mounted into the container and as such any files it writes will have their permissions changed.
+  #   This will change the permissions back and clean up other files we don't want hanging around.
+  sudo chown -R "$(whoami)":"$(whoami)" -- * .*
+  find . -name '*terraform.tfstate*' -exec rm {} \;
+  find . -name '.terraform' -type d -prune -exec rm -rf {} \;
+}
+
 declare ID_RSA_CONTENTS_BASE64
 declare AWS_CREDENTIALS_CONTENTS_BASE64
 declare MAINKEYPAIR_CONTENTS_BASE64
@@ -18,7 +27,7 @@ DOCKER_CONFIG_CONTENTS_BASE64=$(base64 ~/.docker/config.json | tr -d '\n') ;
 
 # The local fs is mounted into the container and as such any files it writes will have their permissions changed.
 #   This will change the permissions back and clean up other files we don't want hanging around.
-sudo chown -R "$(whoami)":"$(whoami)" -- *
+sudo chown -R "$(whoami)":"$(whoami)" -- * .*
 find . -name '*terraform.tfstate*' -exec rm {} \;
 find . -name '.terraform' -type d -prune -exec rm -rf {} \;
 
@@ -27,9 +36,3 @@ docker build infra/build -t conjob-build-test && \
   --runtime=sysbox-runc \
   --volume "$PWD:/opt/build/conjob" \
   conjob-build-test '{"ID_RSA": "'"$ID_RSA_CONTENTS_BASE64"'", "AWS_CREDENTIALS": "'"$AWS_CREDENTIALS_CONTENTS_BASE64"'", "MAIN_KEY_PAIR": "'"$MAINKEYPAIR_CONTENTS_BASE64"'", "DOCKER_CONFIG": "'"$DOCKER_CONFIG_CONTENTS_BASE64"'"}'
-
-# The local fs is mounted into the container and as such any files it writes will have their permissions changed.
-#   This will change the permissions back and clean up other files we don't want hanging around.
-sudo chown -R "$(whoami)":"$(whoami)" -- *
-find . -name '*terraform.tfstate*' -exec rm {} \;
-find . -name '.terraform' -type d -prune -exec rm -rf {} \;
