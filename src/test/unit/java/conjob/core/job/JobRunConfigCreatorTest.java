054ddaefeeb1e6bd5320f4fb003da4810bff758b
Scott Giminiani <scottg489@gmail.com>
2021-04-16 09:16:07 -0700

    Test job run config creation

---
+++ src/test/unit/java/conjob/core/job/JobRunConfigCreatorTest.java
@@ -0,0 +1,61 @@
+package conjob.core.job;
+
+
+import conjob.core.job.model.JobRunConfig;
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.NotEmpty;
+import net.jqwik.api.lifecycle.BeforeTry;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+class JobRunConfigCreatorTest {
+    private JobRunConfigCreator jobRunConfigCreator;
+
+    @BeforeTry
+    void beforeEach() {
+        jobRunConfigCreator = new JobRunConfigCreator();
+    }
+
+    @Property
+    @Label("Given an image name, " +
+            "and not null or empty input, " +
+            "and secret volume name, " +
+            "when getting the container config, " +
+            "should have all the same input values.")
+    void getContainerConfig(
+            @ForAll String givenImageName,
+            @ForAll @NotEmpty String givenInput,
+            @ForAll String givenSecretId) {
+        JobRunConfig containerConfig =
+                jobRunConfigCreator.getContainerConfig(givenImageName, givenInput, givenSecretId);
+
+        assertThat(containerConfig.getJobName(), is(givenImageName));
+        assertThat(containerConfig.getInput(), is(givenInput));
+        assertThat(containerConfig.getSecretsVolumeName(), is(givenSecretId));
+    }
+
+    @Property
+    @Label("Given an image name, " +
+            "and a null or empty input, " +
+            "and secret volume name, " +
+            "when getting the container config, " +
+            "should have all the same input values.")
+    void getContainerConfigNoInput(
+            @ForAll String givenImageName,
+            @ForAll("nullOrEmpty") String givenInput,
+            @ForAll String givenSecretId) {
+        JobRunConfig containerConfig =
+                jobRunConfigCreator.getContainerConfig(givenImageName, givenInput, givenSecretId);
+
+        assertThat(containerConfig.getJobName(), is(givenImageName));
+        assertThat(containerConfig.getInput(), is(nullValue()));
+        assertThat(containerConfig.getSecretsVolumeName(), is(givenSecretId));
+    }
+
+    @Provide
+    Arbitrary<String> nullOrEmpty() {
+        return Arbitraries.just("").injectNull(.5);
+    }
+}
