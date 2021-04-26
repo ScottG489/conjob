71203cd0bfa7b65ecc03c9b566adbfdc922728d6
Scott Giminiani <scottg489@gmail.com>
2021-04-25 21:28:26 -0700

    Remove unnecessary constructors on JobRunResponse
    I possibly thought I needed these here for serialization but it doesn't
    appear to be the case.

--- src/main/java/conjob/api/JobRunResponse.java
+++ src/main/java/conjob/api/JobRunResponse.java
@@ -1,12 +1,8 @@
 package conjob.api;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
-@NoArgsConstructor(force = true)
-@AllArgsConstructor
 public class JobRunResponse {
     JobRunConclusionResponse conclusion;
     String output;
