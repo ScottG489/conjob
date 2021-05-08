82392042b69add2c2b0a0205990e722a09c4dd02
Scott Giminiani <scottg489@gmail.com>
2021-05-07 23:36:10 -0700

    Default limit config to have no limits
    Note that this is for the config object itself. The config file sets the
    defaults on the running application if specified.

--- src/main/java/conjob/config/JobConfig.java
+++ src/main/java/conjob/config/JobConfig.java
@@ -14,9 +14,11 @@ public class JobConfig {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class LimitConfig {
-        private Long maxGlobalRequestsPerSecond;
-        private Long maxConcurrentRuns;
-        private Long maxTimeoutSeconds;
-        private Long maxKillTimeoutSeconds;
+        private Long maxGlobalRequestsPerSecond = Long.MAX_VALUE;
+        private Long maxConcurrentRuns = Long.MAX_VALUE;
+        private Long maxTimeoutSeconds = Long.MAX_VALUE;
+        // TODO: This field should be an integer but complications with ConfigTask and it's tests
+        // TODO:   are preventing this right now.
+        private Long maxKillTimeoutSeconds = (long) Integer.MAX_VALUE;
     }
 }
