b058a354686a4af15222f606dfd4ca3422dd97ed
Scott Giminiani <scottg489@gmail.com>
2021-05-16 17:48:53 -0700

    Reformat integration test code

--- src/test/integration/java/conjob/job/DockerAdapterListVolumesTest.java
+++ src/test/integration/java/conjob/job/DockerAdapterListVolumesTest.java
@@ -13,7 +13,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 
 public class DockerAdapterListVolumesTest {
     private static final String EXISTING_VOLUME_NAME = "conjob-test-volume-870dbba3";
