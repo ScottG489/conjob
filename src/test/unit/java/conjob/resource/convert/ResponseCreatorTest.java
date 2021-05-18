74cf7021a920ada2f7fdd425bd423d4a6ec14c84
Scott Giminiani <scottg489@gmail.com>
2021-05-18 10:24:49 -0700

    Test behavior of unknown conclusion response
    This tests the fallback behavior for when an enum isn't found in the
    map.

--- src/test/unit/java/conjob/resource/convert/ResponseCreatorTest.java
+++ src/test/unit/java/conjob/resource/convert/ResponseCreatorTest.java
@@ -72,6 +72,9 @@ class ResponseCreatorTest {
                 Arbitraries.of(Map.entry(JobRunConclusionResponse.FAILURE, Response.status(Response.Status.BAD_REQUEST).build().getStatusInfo().toEnum())),
                 Arbitraries.of(Map.entry(JobRunConclusionResponse.NOT_FOUND, Response.status(Response.Status.NOT_FOUND).build().getStatusInfo().toEnum())),
                 Arbitraries.of(Map.entry(JobRunConclusionResponse.REJECTED, Response.status(Response.Status.SERVICE_UNAVAILABLE).build().getStatusInfo().toEnum())),
-                Arbitraries.of(Map.entry(JobRunConclusionResponse.TIMED_OUT, Response.status(Response.Status.REQUEST_TIMEOUT).build().getStatusInfo().toEnum())));
+                Arbitraries.of(Map.entry(JobRunConclusionResponse.TIMED_OUT, Response.status(Response.Status.REQUEST_TIMEOUT).build().getStatusInfo().toEnum())),
+                Arbitraries.of(Map.entry(
+                        JobRunConclusionResponse.UNKNOWN,
+                        Response.status(Response.Status.INTERNAL_SERVER_ERROR).build().getStatusInfo().toEnum())));
     }
 }
