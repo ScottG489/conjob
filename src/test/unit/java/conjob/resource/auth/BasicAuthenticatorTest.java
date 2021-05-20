12aa594e81bcac86778c65cadbee953a0964f621
Scott Giminiani <scottg489@gmail.com>
2021-05-20 11:39:23 -0700

    Increase test coverage for basic auth test
    Cases such as usernames being equal but passwords different were not
    being covered to exercise all brances of credential verification.
    
    - Assert name of the principal is the same as the creds

--- src/test/unit/java/conjob/resource/auth/BasicAuthenticatorTest.java
+++ src/test/unit/java/conjob/resource/auth/BasicAuthenticatorTest.java
@@ -1,15 +1,14 @@
 package conjob.resource.auth;
 
 import io.dropwizard.auth.basic.BasicCredentials;
-import net.jqwik.api.Assume;
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Label;
-import net.jqwik.api.Property;
+import net.jqwik.api.*;
 import net.jqwik.api.constraints.UseType;
 import org.eclipse.jetty.security.AbstractLoginService;
 
 import java.util.Optional;
 
+import static net.jqwik.api.Arbitraries.oneOf;
+import static net.jqwik.api.Arbitraries.strings;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -25,6 +24,7 @@ class BasicAuthenticatorTest {
                         .authenticate(basicCreds);
 
         assertThat(userPrincipal.isPresent(), is(true));
+        assertThat(userPrincipal.get().getName(), is(basicCreds.getUsername()));
     }
 
     @Property
@@ -33,14 +33,40 @@ class BasicAuthenticatorTest {
             "when the latter is authenticated against the former, " +
             "then the authentication should not return a user principal.")
     void authenticateIncorrectCreds(
-            @ForAll @UseType BasicCredentials basicCreds,
-            @ForAll @UseType BasicCredentials differentCreds) {
-        Assume.that(!basicCreds.equals(differentCreds));
+            @ForAll("differentCreds") Tuple.Tuple2<BasicCredentials, BasicCredentials> differentCreds) {
+        BasicCredentials basicCreds = differentCreds.get1();
+        BasicCredentials otherCreds = differentCreds.get2();
 
         Optional<AbstractLoginService.UserPrincipal> userPrincipal =
                 new BasicAuthenticator(basicCreds.getUsername(), basicCreds.getPassword())
-                        .authenticate(differentCreds);
+                        .authenticate(otherCreds);
 
         assertThat(userPrincipal.isPresent(), is(false));
+    }
+
+    @Provide
+    Arbitrary<Tuple.Tuple2<BasicCredentials, BasicCredentials>> differentCreds() {
+        return oneOf(sameUserDiffPassCreds(), samePassDiffUsernameCreds(), allDifferentCreds());
+    }
+
+    private Arbitrary<Tuple.Tuple2<BasicCredentials, BasicCredentials>> sameUserDiffPassCreds() {
+        return Combinators.combine(strings(), strings(), strings())
+                .as((userA, passA, passB) ->
+                        Tuple.of(new BasicCredentials(userA, passA), new BasicCredentials(userA, passB)))
+                .filter(credPair -> !credPair.get1().equals(credPair.get2()));
+    }
+
+    private Arbitrary<Tuple.Tuple2<BasicCredentials, BasicCredentials>> samePassDiffUsernameCreds() {
+        return Combinators.combine(strings(), strings(), strings())
+                .as((userA, passA, userB) ->
+                        Tuple.of(new BasicCredentials(userA, passA), new BasicCredentials(userB, passA)))
+                .filter(credPair -> !credPair.get1().equals(credPair.get2()));
+    }
+
+    private Arbitrary<Tuple.Tuple2<BasicCredentials, BasicCredentials>> allDifferentCreds() {
+        return Combinators.combine(strings(), strings(), strings(), strings())
+                .as((userA, passA, userB, passB) ->
+                        Tuple.of(new BasicCredentials(userA, passA), new BasicCredentials(userB, passA)))
+                .filter(credPair -> !credPair.get1().equals(credPair.get2()));
     }
 }
