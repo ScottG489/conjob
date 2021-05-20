463080ed0f4c37a4d951c5fe7a42ab552f559967
Scott Giminiani <scottg489@gmail.com>
2021-05-19 19:55:10 -0700

    Mock MDC calls to make tests more stable
    I believe the MDC is scoped to the entire JVM and persists state until
    all tests are finished. Whatever the case, these tests were very flakey
    due to the fact that we were using the real MDC which is better off
    mocked anyways.
    
    This takes advantage of static mocks which are a little complicated, but
    unfortunately necessary in this case.

--- src/test/unit/java/conjob/resource/filter/EveryResponseFilterTest.java
+++ src/test/unit/java/conjob/resource/filter/EveryResponseFilterTest.java
@@ -3,8 +3,8 @@ package conjob.resource.filter;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Label;
 import net.jqwik.api.Property;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.slf4j.MDC;
 
 import javax.ws.rs.container.ContainerRequestContext;
@@ -17,18 +17,22 @@ class EveryResponseFilterTest {
     private static final String TRACE_ID_NAME = "traceId";
 
     @Test
-    @DisplayName("Given the filter, " +
+    @Label("Given the filter, " +
             "and no MDC trace id exists, " +
             "when it's filtering a response, " +
             "should add the wildcard allow origin header to the response.")
     void addAllowOriginHeader() {
         EveryResponseFilter everyResponseFilter = new EveryResponseFilter();
         ContainerResponseContext resp = mock(ContainerResponseContext.class, RETURNS_DEEP_STUBS);
+        try (MockedStatic<MDC> mockMdc = mockStatic(MDC.class)) {
+            mockMdc.when(() -> MDC.get(TRACE_ID_NAME))
+                    .thenReturn(null);
 
-        everyResponseFilter.filter(mock(ContainerRequestContext.class), resp);
+            everyResponseFilter.filter(mock(ContainerRequestContext.class), resp);
 
-        verify(resp.getHeaders(), times(1)).add("Access-Control-Allow-Origin", "*");
-        verify(resp.getHeaders(), times(0)).add(eq(TRACE_ID_HEADER_NAME), any());
+            verify(resp.getHeaders(), times(1)).add("Access-Control-Allow-Origin", "*");
+            verify(resp.getHeaders(), times(0)).add(eq(TRACE_ID_HEADER_NAME), any());
+        }
     }
 
     @Property
@@ -39,11 +43,14 @@ class EveryResponseFilterTest {
     void addTraceIdIfPresent(@ForAll String givenTraceId) {
         EveryResponseFilter everyResponseFilter = new EveryResponseFilter();
         ContainerResponseContext resp = mock(ContainerResponseContext.class, RETURNS_DEEP_STUBS);
+        try (MockedStatic<MDC> mockMdc = mockStatic(MDC.class)) {
+            mockMdc.when(() -> MDC.get(TRACE_ID_NAME))
+                    .thenReturn(givenTraceId);
 
-        MDC.put(TRACE_ID_NAME, givenTraceId);
-        everyResponseFilter.filter(mock(ContainerRequestContext.class), resp);
+            everyResponseFilter.filter(mock(ContainerRequestContext.class), resp);
 
-        verify(resp.getHeaders(), times(1)).add(eq(TRACE_ID_HEADER_NAME), any());
-        MDC.clear();
+            verify(resp.getHeaders(), times(1)).add("Access-Control-Allow-Origin", "*");
+            verify(resp.getHeaders(), times(1)).add(TRACE_ID_HEADER_NAME, givenTraceId);
+        }
     }
 }
