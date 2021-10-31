00f27833bd34f34bce8e5e7d6d4f2d18ac1e8ff1
Scott Giminiani <scottg489@gmail.com>
2021-10-31 19:38:26 -0400

    Sync HTTPS changes with alt-env

--- infra/alt-env/tf/modules/conjob_core/outputs.tf
+++ infra/alt-env/tf/modules/conjob_core/outputs.tf
@@ -2,6 +2,10 @@ output "r53_zone_name" {
   value = aws_route53_zone.r53_zone.name
 }
 
+output "r53_zone_id" {
+  value = aws_route53_zone.r53_zone.id
+}
+
 output "r53_zone_name_servers" {
   value = aws_route53_zone.r53_zone.name_servers
 }
