2e5c5addf1306582d2ac05eb36a7cbe50f95b982
Scott Giminiani <scottg489@gmail.com>
2021-11-09 01:08:05 -0500

    Use the domain name for the name of the alt-env
    This domain currently being alt.conjob.io

--- infra/alt-env/tf/main.tf
+++ infra/alt-env/tf/main.tf
@@ -13,7 +13,7 @@ terraform {
 module "helpers_spot_instance_ssh" {
   source = "ScottG489/helpers/aws//modules/spot_instance_ssh"
   version = "0.1.4"
-  name = "alt-${var.subdomain_name}.${var.second_level_domain_name}.${var.top_level_domain_name}"
+  name = "${var.subdomain_name}.${var.second_level_domain_name}.${var.top_level_domain_name}"
   instance_type = var.instance_type
   spot_type = var.spot_type
   spot_price = var.spot_price
