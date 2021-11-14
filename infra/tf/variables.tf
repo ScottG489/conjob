281be16cd1512f0d3b83bc0ae38233dbd93d6cee
Scott Giminiani <scottg489@gmail.com>
2021-11-14 13:36:01 -0500

    Remove unused variable
    This variable was used when we were passing in the keystore password as
    a secret. Now it's generated randomly and isn't needed anymore

--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -2,6 +2,3 @@ variable "second_level_domain_name" {}
 variable "top_level_domain_name" {}
 variable "subdomain_name" { }
 variable "public_key" {}
-variable "keystore_password" {
-  sensitive = true
-}
