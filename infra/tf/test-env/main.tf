a8b199427f8b4985c2314d33d6e773f05da182b9
Scott Giminiani <scottg489@gmail.com>
2022-03-12 01:47:22 -0500

    Implement static IP changes into terraform test env
    These changes are being pulled over from the alt-env. We'd like to have
    the option to restart the server easily if needed even if it isn't as
    necessary as it is in the alt-env.
    
    These changes will also be added to the prod env.

--- infra/tf/test-env/main.tf
+++ infra/tf/test-env/main.tf
@@ -4,10 +4,11 @@ provider "aws" {
 
 module "helpers_spot_instance_ssh" {
   source = "ScottG489/helpers/aws//modules/spot_instance_ssh"
-  version = "0.0.4"
+  version = "0.1.11"
   name = "${var.subdomain_name}.${random_id.second_level_domain_name.hex}.${var.top_level_domain_name}"
   instance_type = var.instance_type
   spot_type = var.spot_type
+  instance_interruption_behavior = var.instance_interruption_behavior
   spot_price = var.spot_price
   volume_size = var.volume_size
   public_key = var.public_key
@@ -23,4 +24,14 @@ module "conjob" {
 resource "random_id" "second_level_domain_name" {
   byte_length = 4
   prefix = "${var.second_level_domain_name}-"
+}
+
+resource "aws_eip" "eip" {
+  vpc      = true
+}
+
+# The association is necessary because the aws_eip resource requires the instance be available, which it may not be since it's a spot instance.
+resource "aws_eip_association" "eip_assoc" {
+  instance_id = module.helpers_spot_instance_ssh.spot_instance_id
+  allocation_id = aws_eip.eip.id
 }
