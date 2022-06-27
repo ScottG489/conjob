f3ced13a860c52e93147e330562b3dfb7171dbe4
Scott Giminiani <scottg489@gmail.com>
2022-06-27 11:13:30 -0400

    Don't output alt-env script inputs
    These are all secrets so they probably shouldn't be output, although
    it's only meant to be run locally so it doesn't really matter. However,
    there's a lot of output and it's useless to print all of it to the
    terminal every run and is annoying to scroll past.

--- alt-env.sh
+++ alt-env.sh
@@ -7,6 +7,7 @@ declare MAINKEYPAIR_CONTENTS_BASE64
 declare DOCKER_CONFIG_CONTENTS_BASE64
 
 # Change the location of these files based on where they are on your system
+set +x
 ID_RSA_CONTENTS_BASE64=$(base64 ~/.ssh/id_rsa | tr -d '\n') ;
 AWS_CREDENTIALS_CONTENTS_BASE64=$(base64 ~/.aws/credentials | tr -d '\n') ;
 MAINKEYPAIR_CONTENTS_BASE64=$(base64 ~/.ssh/mainkeypair.pem | tr -d '\n') ;
@@ -15,6 +16,7 @@ DOCKER_CONFIG_CONTENTS_BASE64=$(base64 ~/.docker/config.json | tr -d '\n') ;
 [[ -n $AWS_CREDENTIALS_CONTENTS_BASE64 ]]
 [[ -n $MAINKEYPAIR_CONTENTS_BASE64 ]]
 [[ -n $DOCKER_CONFIG_CONTENTS_BASE64 ]]
+set -x
 
 # The local fs is mounted into the container and as such any files it writes will have their permissions changed.
 #   This will change the permissions back and clean up other files we don't want hanging around.
