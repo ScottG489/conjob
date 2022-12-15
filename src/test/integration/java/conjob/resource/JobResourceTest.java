d58561b4671f4e57fa9ab38eb1ab0decac801ed2
Scott Giminiani <scottg489@gmail.com>
2022-12-14 23:33:46 -0500

    Test that we allow image names even if they don't have tags
    Integration test for the new default tag functionality.

---
+++ src/test/integration/java/conjob/resource/JobResourceTest.java
@@ -0,0 +1,42 @@
+package conjob.resource;
+
+import conjob.ConJobApplication;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.startsWith;
+
+public class JobResourceTest {
+    private static final String JOB_RUN_PATH = "/job/run";
+
+    private ConJobApplication app;
+
+    @BeforeEach
+    public void beforeEach() {
+        app = new ConJobApplication();
+    }
+
+    @AfterEach
+    public void afterEach() throws Exception {
+        app.getEnvironment().getApplicationContext().getServer().stop();
+    }
+
+    @Test
+    public void getPlainTextResponseWithoutTag() throws Exception {
+        String expectStartsWith = "\nHello from Docker!";
+
+        app.run("server", "src/test/integration/resources/default_config.yml");
+
+        given()
+                .get(JOB_RUN_PATH + "?image=library/hello-world")
+        .then()
+                .statusCode(HttpStatus.SC_OK)
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(startsWith(expectStartsWith));
+    }
+}
