ee08b26f00035d437ebe120419ad9125e5907df7
Scott Giminiani <scottg489@gmail.com>
2026-02-26 18:43:33 -0500

    Specify versions in versions.tf file
    Versions were pinned in the lockfile but we should also specify them
    here.

--- infra/tf/versions.tf
+++ infra/tf/versions.tf
@@ -6,7 +6,8 @@ terraform {
   }
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
     acme = {
       source  = "vancluever/acme"
