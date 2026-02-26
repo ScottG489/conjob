dec294413fe03f112e7e79794cc7445b5e148059
Scott Giminiani <scottg489@gmail.com>
2026-02-26 18:43:33 -0500

    Specify versions in versions.tf file
    Versions were pinned in the lockfile but we should also specify them
    here.

---
+++ infra/tf/test-env/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
